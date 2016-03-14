c2b5d84965389729d30d12de422ed60429efa3ac
Marc Sances <marcsances@gmail.com>
2016-03-14 12:01:18 +0100

    el hellloworld

--- sobres/sobres/urls.py
+++ sobres/sobres/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-
+from isobres.views import mainpage
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', mainpage),
 ]
